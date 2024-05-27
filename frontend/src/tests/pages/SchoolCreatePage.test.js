86ab31c5bf7e3c7ae7f98ba6d2c8dc6679adf333
Shuhan Lyu <shuhanlyu@ucsb.edu>
2024-05-27 15:11:45 -0700

    fix eslint error

--- frontend/src/tests/pages/SchoolCreatePage.test.js
+++ frontend/src/tests/pages/SchoolCreatePage.test.js
@@ -89,7 +89,7 @@ describe("SchoolCreatePage tests", () => {
        
         fireEvent.change(abbrevField, { target: { value: 'abb' } });
         fireEvent.change(nameField, { target: { value: 'name-1' } });
-        fireEvent.change(termRegexField, { target: { value: '[WSMF]\d\d' } });
+        fireEvent.change(termRegexField, { target: { value: '[WSMF]\\d\\d' } });
         fireEvent.change(termDescriptionField, { target: { value: 'F23' } });
         fireEvent.change(termErrorField, { target: { value: 'error-1' } });
       
@@ -104,7 +104,7 @@ describe("SchoolCreatePage tests", () => {
             {
                 "abbrev": "abb",
                 "name": "name-1",
-                "termRegex": "[WSMF]\d\d",
+                "termRegex": "[WSMF]\\d\\d",
                 "termDescription": "F23",
                 "termError": "error-1"
         });
