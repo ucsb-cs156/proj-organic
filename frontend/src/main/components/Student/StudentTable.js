120610492d10c55881a4428938626438fb9bd380
Rick T. Zhang <rtzhang@ucsb.edu>
2024-05-27 19:41:15 +0000

    rt.z - Made the StduentTable.js

---
+++ frontend/src/main/components/Student/StudentTable.js
@@ -0,0 +1,49 @@
+import React from "react";
+import OurTable from "main/components/OurTable"
+
+
+ export default function StaffTable({ students }) {
+
+     // Stryker disable next-line all : TODO try to make a good test for this
+
+     const columns = [
+         {
+             Header: 'id',
+             accessor: 'id',
+         },
+         {
+             Header: 'courseId',
+             accessor: 'courseId',
+         },
+         {
+             Header: 'githubId',
+             accessor: 'githubId',
+         },
+         {
+             Header: 'fname',
+             accessor: 'fname',
+         },
+         {
+            Header: 'lname',
+            accessor: 'lname',
+         },
+         {
+            Header: 'studentId',
+            accessor:'studentId',
+         },
+         {
+            Header: 'email',
+            accessor: 'email',
+         },
+         {
+            Header: 'githubId',
+            accessor: 'githubId',
+         }
+  
+     ];
+
+     return <OurTable
+         data={students}
+         columns={columns}
+         testid={"StudentTable"} />;
+    };
