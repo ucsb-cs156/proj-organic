5fe8def405a535a57b21dbf4b6187591b831e95c
ukyl <53740464+ukyl@users.noreply.github.com>
2024-05-28 13:51:10 -0700

    fix eslint errors

--- frontend/src/tests/components/OurAddDropdownForm.test.js
+++ frontend/src/tests/components/OurAddDropdownForm.test.js
@@ -15,6 +15,11 @@ describe('OurAddDropdownForm Tests', () => {
         { label: 'a', key: 'a1' },
         { label: 'a', key: 'a2' },   
     ];
+    const subsetThreeOptions = [
+        { label: 'ab', key: 'ab' },
+        { label: 'abba', key: 'abba' },
+        { label: 'abba concert', key: 'abba concert' },
+    ];
 
     test('renders an empty dropdown element without crashing', () => {
         render(
@@ -53,9 +58,9 @@ describe('OurAddDropdownForm Tests', () => {
         fireEvent.select(submitField);
         // verify that initial selection is highlighted
         expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
-        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor: "white"});
-        expect(await screen.findByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor: "green"});
-        expect(await screen.findByTestId('testid-dropdown-form-option-2')).toHaveStyle({backgroundColor: "white"});
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor: "green"});
+        expect(screen.queryByTestId('testid-dropdown-form-option-1')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-2')).not.toBeInTheDocument();
     });
 
     test('renders on no inital selected element', async () => {
@@ -174,12 +179,13 @@ describe('OurAddDropdownForm Tests', () => {
         const selectOption = screen.getByTestId('testid-dropdown-form-option-1');
 
         fireEvent.click(selectOption);
-        fireEvent.select(submitField);
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        fireEvent.click(submitField);
 
         expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
-        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor: "white"});
-        expect(await screen.findByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor: "green"});
-        expect(await screen.findByTestId('testid-dropdown-form-option-2')).toHaveStyle({backgroundColor: "white"});
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor: "green"});
+        expect(screen.queryByTestId('testid-dropdown-form-option-1')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-2')).not.toBeInTheDocument();
     });
 
     test("selection color changes over hovering", async () => {
@@ -198,12 +204,12 @@ describe('OurAddDropdownForm Tests', () => {
         const selectOption = screen.getByTestId('testid-dropdown-form-option-2');
         fireEvent.mouseOver(selectOption);
 
-        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor: "white"});
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor: "lightgreen"}); // ghost
         expect(await screen.findByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor: "white"});
         expect(await screen.findByTestId('testid-dropdown-form-option-2')).toHaveStyle({backgroundColor: "lightgreen"});
 
         fireEvent.mouseOut(selectOption);
-        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor: "white"});
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor: "lightgreen"}); // ghost
         expect(await screen.findByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor: "white"});
         expect(await screen.findByTestId('testid-dropdown-form-option-2')).toHaveStyle({backgroundColor: "white"});
     });
@@ -218,6 +224,7 @@ describe('OurAddDropdownForm Tests', () => {
         expect(await screen.findByTestId('testid-dropdown-form-option-0')).toHaveTextContent("b");
         expect(await screen.findByTestId('testid-dropdown-form-option-1')).toHaveTextContent("c");
     });
+
 
     test("same label options are rendered", async () => {
         render(<OurAddDropdownForm content={sameTwoOptions} label="empty" />);
@@ -236,5 +243,232 @@ describe('OurAddDropdownForm Tests', () => {
         expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
         expect(screen.getByTestId('testid-test-dropdown-form')).toHaveAttribute("disabled");
         expect(screen.getByTestId('testid-test-dropdown-form')).toHaveStyle({"cursor" : "not-allowed"});
-    })
+    });
+
+    test("prefix renders only a portion of options", async () => {
+        render(<OurAddDropdownForm content={threeOptions} label="empty" />);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : 'c'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toBeInTheDocument();
+        expect(await screen.findByTestId('testid-dropdown-form-option-1')).toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-2')).not.toBeInTheDocument();
+
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveTextContent("choose me");
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveTextContent("click me");
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor:"lightgreen"}); // ghost
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor:"white"});
+    });
+
+    test("prefix renders only a portion of options 2", async () => {
+        render(<OurAddDropdownForm content={threeOptions} label="empty" />);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : 'p'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-1')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-2')).not.toBeInTheDocument();
+
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveTextContent("pick me");
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor:"lightgreen"});
+    });
+
+    test("prefix renders only a portion of options with an initial selection", async () => {
+        render(<OurAddDropdownForm content={subsetThreeOptions} label="empty" basis={subsetThreeOptions[1]}/>);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-2')).not.toBeInTheDocument();
+
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveTextContent("abba");
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor:"green"});
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveTextContent("abba concert");
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor:"white"});
+    });
+
+    test("prefix renders only a portion of options with an initial selection 2", async () => {
+        render(<OurAddDropdownForm content={subsetThreeOptions} label="empty" basis={subsetThreeOptions[0]}/>);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toBeInTheDocument();
+
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveTextContent("ab");
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor:"green"});
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveTextContent("abba");
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor:"white"});
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toHaveTextContent("abba concert");
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toHaveStyle({backgroundColor:"white"});
+    });
+
+    test("prefix renders only a portion of options 3", async () => {
+        render(<OurAddDropdownForm content={subsetThreeOptions} label="empty"/>);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : 'a'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toBeInTheDocument();
+
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveTextContent("a");
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor:"lightgreen"});
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveTextContent("abba");
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor:"white"});
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toHaveTextContent("abba concert");
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toHaveStyle({backgroundColor:"white"});
+    });
+
+    test("prefix renders only a portion of options after changing prefix and the initialElement is no longer selected", async () => {
+        render(<OurAddDropdownForm content={subsetThreeOptions} label="empty" basis={subsetThreeOptions[0]}/>);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toBeInTheDocument();
+
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveTextContent("ab");
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor:"green"});
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveTextContent("abba");
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor:"white"});
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toHaveTextContent("abba concert");
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toHaveStyle({backgroundColor:"white"});
+
+        fireEvent.change(submitField, { target: { value : 'a'} });
+        fireEvent.click(submitField); // select doesn't work for some reason?
+
+        expect(await screen.findByTestId('testid-dropdown-form-option-0')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toBeInTheDocument();
+
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveTextContent("ab");
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toHaveStyle({backgroundColor:"lightgreen"});
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveTextContent("abba");
+        expect(screen.getByTestId('testid-dropdown-form-option-1')).toHaveStyle({backgroundColor:"white"});
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toHaveTextContent("abba concert");
+        expect(screen.getByTestId('testid-dropdown-form-option-2')).toHaveStyle({backgroundColor:"white"});
+    });
+
+    test("prefix removes all options and causes red text", async () => {
+        render(<OurAddDropdownForm content={threeOptions} label="empty" />);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : 'cheese'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-0')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-1')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-2')).not.toBeInTheDocument();
+
+        expect(submitField).toHaveStyle({color:"red"});
+    });
+
+    test("red text removed on proper prefix", async () => {
+        render(<OurAddDropdownForm content={threeOptions} label="empty" />);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : 'cheese'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-0')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-1')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-2')).not.toBeInTheDocument();
+
+        expect(submitField).toHaveStyle({color:"red"});
+
+        fireEvent.change(submitField, { target: { value : 'choose'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+        expect(screen.getByTestId('testid-dropdown-form-option-0')).toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-1')).not.toBeInTheDocument();
+        expect(screen.queryByTestId('testid-dropdown-form-option-2')).not.toBeInTheDocument();
+
+        expect(submitField).not.toHaveStyle({color:"red"});
+    });
+
+    test("enter autocompletes and chooses first selection", async () => {
+        render(<OurAddDropdownForm content={threeOptions} label="empty" />);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : 'choose'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+        fireEvent.keyDown(submitField, {key: 'Enter', code: 'Enter'});
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        expect(submitField).toHaveAttribute("value", "choose me");
+        expect(screen.queryByTestId('testid-wrapper')).not.toBeInTheDocument();
+    });
+
+    test("dropdown still shows on keyboard input aside from Enter", async () => {
+        render(<OurAddDropdownForm content={threeOptions} label="empty" />);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : ''} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+        fireEvent.keyDown(submitField, {key: 'c', code: 'c'});
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+    });
+
+    test("dropdown still shows on autocomplete attempt with no valid options", async () => {
+        render(<OurAddDropdownForm content={threeOptions} label="empty" />);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : 'I SEE FIRE'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+        fireEvent.keyDown(submitField, {key: 'Enter', code: 'Enter'});
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+    });
+
+    test("dropdown rerenders after autocomplete and nonEnterKey", async () => {
+        render(<OurAddDropdownForm content={threeOptions} label="empty" />);
+
+        expect(await screen.findByTestId('testid-test-dropdown-form')).toBeInTheDocument();
+        const submitField = screen.getByTestId('testid-test-dropdown-form');
+        fireEvent.change(submitField, { target: { value : 'c'} });
+        fireEvent.select(submitField);
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+        fireEvent.keyDown(submitField, {key: 'Enter', code: 'Enter'});
+
+        expect(screen.queryByTestId('testid-wrapper')).not.toBeInTheDocument();
+        fireEvent.keyDown(submitField, {key: 'Esc', code: 'Esc'});
+
+        expect(await screen.findByTestId('testid-wrapper')).toBeInTheDocument();
+    });
+
 });
