dac38090236a8b551b03c9877b381b988e8b20ed
Peak <pmanopchantaroj@ucsb.edu>
2024-05-29 19:42:53 -0700

    PM -invalidate queries seem to force the refresh

--- frontend/src/main/components/Staff/StaffTable.js
+++ frontend/src/main/components/Staff/StaffTable.js
@@ -3,17 +3,25 @@ import OurTable, { ButtonColumn } from "main/components/OurTable"
 import { useBackendMutation } from "main/utils/useBackend";
 import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/components/Utils/StaffUtils"
 import { hasRole } from "main/utils/currentUser";
+import { useQueryClient } from "react-query";
 
 
 export default function StaffTable({ staff, currentUser }) {
 
+    const queryClient = useQueryClient(); 
+
     // Stryker disable all : hard to test for query caching
     const deleteMutation = useBackendMutation(
         cellToAxiosParamsDelete,
-        { onSuccess: onDeleteSuccess }
+        {
+            onSuccess: (data) => {
+                onDeleteSuccess(data);
+                queryClient.invalidateQueries(["/api/courses/getStaff"]); 
+            }
+        },
         ["/api/courses/getStaff"]
     );
-    // Stryker restore all 
+    // Stryker restore all
 
     // Stryker disable next-line all : TODO try to make a good test for this
     const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
