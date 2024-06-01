b4668d3e9f92475d239be2535df474a68a9f41f0
Sreeganesh Siva <ssiva@ucsb.edu>
2024-06-01 16:20:44 -0700

    ss - I think this fixes tests for the new version of TermDescription

--- src/main/java/edu/ucsb/cs156/organic/controllers/SchoolController.java
+++ src/main/java/edu/ucsb/cs156/organic/controllers/SchoolController.java
@@ -21,7 +21,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.extern.slf4j.Slf4j;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import java.time.LocalDateTime;
